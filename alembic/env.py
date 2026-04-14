1099d0350bcc77d1e10faff059f517dc0697a2af
alonso-be <alonso-be@gate.me>
2026-04-14 15:44:06 +0800

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,11 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from app.core.config import settings
 from app.core.database import Base
 
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.mysql_dsn)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
